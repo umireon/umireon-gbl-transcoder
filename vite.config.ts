c6d5d4ce7a9c61ca0ba36732ada52115e117a704
Kaito Udagawa <umireon@gmail.com>
2022-06-10 16:40:09 +0900

    Template

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        app: resolve(__dirname, 'app.html'),
+        index: resolve(__dirname, 'index.html'),
+        privacypolicy: resolve(__dirname, 'privacypolicy.html'),
+        termsofservice: resolve(__dirname, 'termsofservice.html'),
+        twitch: resolve(__dirname, 'twitch.html'),
+      },
+      output: {
+        manualChunks: {
+          firebasecompat: ['firebase/compat/app'],
+        },
+      },
+    },
+  },
+  plugins: [svelte()],
+})
