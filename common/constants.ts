fb879b18163c8790c8436769862993f97ac8a129
Kaito Udagawa <umireon@gmail.com>
2022-06-10 16:47:31 +0900

    Login

---
+++ common/constants.ts
@@ -0,0 +1,13 @@
+export interface AppContext {}
+
+export const DEFAULT_CONTEXT: AppContext = {} as const
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBPw7N0RrYVVS62uqR1rpewDBHrgck1jW0',
+  appId: '1:1007712463625:web:12c838b6d7ee50187801d8',
+  authDomain: 'umireon-gbl-transcoder.firebaseapp.com',
+  measurementId: 'G-RS8HWFY60C',
+  messagingSenderId: '1007712463625',
+  projectId: 'umireon-gbl-transcoder',
+  storageBucket: 'umireon-gbl-transcoder.appspot.com',
+} as const
