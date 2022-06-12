75145362339e1427ed0758fd08fb459c074b47d3
Kaito Udagawa <umireon@gmail.com>
2022-06-13 02:38:13 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { DEFAULT_CONTEXT, firebaseConfig } from './common/constants.js'
+import { type Storage, getStorage } from 'firebase-admin/storage'
 
 import { TranscoderServiceClient } from '@google-cloud/video-transcoder'
-import { getStorage } from 'firebase-admin/storage'
 import { handleCors } from './service/cors.js'
 import { http } from '@google-cloud/functions-framework'
 import { initializeApp } from 'firebase-admin/app'
@@ -77,11 +77,25 @@ http('transcode-video', async (req, res) => {
   res.send({ name })
 })
 
+export async function setContentDisposition(storage: Storage, name: string): Promise<boolean> {
+  const bucket = storage.bucket()
+  const file = bucket.file(`transcoded/${name}`)
+  const [fileExists] = await file.exists()
+  if (fileExists) {
+    await file.setMetadata({
+      contentDisposition: 'attachment',
+    })
+    return true
+  } else {
+    return false
+  }
+}
+
 http('check-downloadable', async (req, res) => {
   if (!handleCors(req, res)) return
 
   // Validate query
-  if (typeof req.query.jobName !== 'string') {
+  if (typeof req.query.jobName !== 'undefined' && typeof req.query.jobName !== 'string') {
     res.status(400).send('Invalid jobName')
     return
   }
@@ -91,24 +105,28 @@ http('check-downloadable', async (req, res) => {
   }
   const { jobName, name } = req.query
 
-  const [{ error, state }] = await transcoderServiceClient.getJob({
-    name: jobName
-  })
-  if (state === 'PENDING' || state === 'RUNNING') {
-    res.status(404).send('Video not ready')
-  } else if (state === 'SUCCEEDED') {
-    const bucket = storage.bucket()
-    const file = bucket.file(`transcoded/${name}`)
-    const [fileExists] = await file.exists()
-    if (fileExists) {
-      await file.setMetadata({
-        contentDisposition: 'attachment',
-      })
+  if (typeof jobName === 'undefined') {
+    const exists = await setContentDisposition(storage, name)
+    if (exists) {
       res.status(204).send('')
     } else {
       res.status(500).send('Video not found')
     }
-  } else if (state === 'FAILED') {
-    res.status(500).send(error)
+  } else {
+    const [{ error, state }] = await transcoderServiceClient.getJob({
+      name: jobName
+    })
+    if (state === 'PENDING' || state === 'RUNNING') {
+      res.status(404).send('Video not ready')
+    } else if (state === 'SUCCEEDED') {
+      const exists = await setContentDisposition(storage, name)
+      if (exists) {
+        res.status(204).send('')
+      } else {
+        res.status(500).send('Video not found')
+      }
+    } else if (state === 'FAILED') {
+      res.status(500).send(error)
+    }
   }
 })
